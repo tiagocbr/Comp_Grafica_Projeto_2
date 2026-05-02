a87f389cf0766d769c2aa076a4770ae08ac715ee
tiago <tiagocbr06@hotmail.com>
2026-05-02 16:51:11 -0300

    add mais objetos

--- fragment_shader.fs
+++ fragment_shader.fs
@@ -4,7 +4,7 @@ uniform vec4 color;
 varying vec2 out_texture;
 uniform sampler2D imagem;
 
-void main(){
-	vec4 texture = texture2D(imagem, out_texture);
-	gl_FragColor = texture;
+void main(){ 
+    vec4 texture = texture2D(imagem, out_texture);
+    gl_FragColor = texture; 
 }
